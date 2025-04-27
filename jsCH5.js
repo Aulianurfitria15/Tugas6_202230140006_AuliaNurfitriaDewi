4ad5701ef2a2567470d57ced04b2200c4e74bee3
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-04-27 07:48:32 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,17 @@
+// Ambil elemen dari HTML berdasarkan id
+const foto = document.getElementById("foto");
+const video = document.getElementById("video");
+const audio = document.getElementById("audio");
+
+// Tambahkan event listener untuk masing-masing elemen
+foto.addEventListener("click", function () {
+    alert("Lahir 15 November 2004 dan sekarang eug otw 21 nihh! 💖");
+});
+
+video.addEventListener("click", function () {
+    alert("Video transisi lebaran aku kemarinn nihh! 🎬✨");
+});
+
+audio.addEventListener("click", function () {
+    alert("Darii pacar akoo, uwuu~ 😍🎶");
+});
